40ac5fb8d861ba149d5187d09143825fbd22c634
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-07-12 11:14:10 -0700

    Uploading the print_addresses.c file

--- print_addresses.c
+++ print_addresses.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * print_address - prints address of input in hexa format
- * @l: va_list arguments from _printf
+ * print_address - Prints address of the input in hexa format.
+ *
+ * @l: va_list arguments from _printf.
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf.
+ *
+ * Return: number of char printed.
+ *
  */
+
 int print_address(va_list l, flags_t *f)
 {
 	char *str;
