6eb25d2f5e42f1304ec0d74e3ba0a52667aa3596
Abe Timileyin <oluwatimileyin.abe@gmail.com>
2022-07-12 10:32:34 -0700

    most added

--- _printf.c
+++ _printf.c
@@ -17,31 +17,4 @@ int _printf(const char *format, ...)
 
 	register int count = 0;
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
 }
