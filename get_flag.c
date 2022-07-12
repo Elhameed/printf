cdf53183e01f3450ee39a1a6140c2defb61f66ab
A-OT <oluwatimileyin.abe@gmail.com>
2022-07-12 19:18:31 +0100

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * get_flag - turns on flags if _printf finds
- * a flag modifier in the format string
- * @s: character that holds the flag specifier
- * @f: pointer to the struct flags in which we turn the flags on
+ * get_flag - Turns on flags if _printf finds
+ * a flag modifier in the format string.
  *
- * Return: 1 if a flag has been turned on, 0 otherwise
+ * @s: Character that holds the flag specifier.
+ * @f: Pointer to the struct flags in which we turn the flags on.
+ *
+ * Return: 1 if a flag has been turned on, 0 otherwise.
+ *
  */
+
 int get_flag(char s, flags_t *f)
 {
 	int i = 0;
